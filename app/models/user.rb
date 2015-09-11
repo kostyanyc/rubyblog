57a146fe6648686f8ca044682c6b4a8b8529d8de
Konstantin Mikhaylov <kmikhaylov@createthegroup.com>
2015-09-11 15:38:34 -0400

    adding test cases to the project

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  #validations
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true,
+                           uniqueness: true,
+                           format: {
+                           	with: /\A[a-zA-Z0-9]+\z/,
+                           	message: "Must be formatted correctly"
+                           }
+
   has_many :statuses
 
 
