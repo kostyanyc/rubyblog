e1dfe38d7bf92f11e6a05090baf58b2544815b84
Konstantin Mikhaylov <kmikhaylov@createthegroup.com>
2015-09-16 10:41:47 -0400

    commiting routes m

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
 
 
-  # Api definition
-  namespace :api, defaults: { format: :json } do
-    # We are going to list our resources here
-  end
-
-
   resources :statuses
   root 'statuses#index'
   
