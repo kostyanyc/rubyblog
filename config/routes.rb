bf4f351499b56cbe3f5f7d74f85cf7b59db64bd6
Konstantin Mikhaylov <kmikhaylov@createthegroup.com>
2015-09-17 12:39:08 -0400

    Revert "commiting routes"
    This reverts commit e1dfe38d7bf92f11e6a05090baf58b2544815b84.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
 
+  # Api definition
+  namespace :api, defaults: { format: :json } do
+    # We are going to list our resources here
+  end
+
+
   resources :statuses
   root 'statuses#index'
   
